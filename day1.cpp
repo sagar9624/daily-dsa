80a6f332dc4d5704cc6d87ade0ec66777554d123
Sagar B R <sagarsagar34176@gmail.com>
2026-03-28 10:45:40 +0530

    Implement selection sort in day1.cpp
    Added Day 1-selection sort

---
+++ day1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+void selectionSort(int arr[],int n){
+  for(int i=0;i<=n-1;i++){
+    int min_index=i;
+    for(int j=i+1;j<n;j++){
+      if(arr[j]<arr[min_index])min_index=j;
+  }
+    int temp=arr[i];
+    arr[i]=arr[min_index];
+    arr[min_index]=temp;
+  }
+
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)cin>>arr[i];
+  selectionSort(arr,n);
+  for(int i=0;i<n;i++)cout<<arr[i]<<" ";
+  return 0;
+}

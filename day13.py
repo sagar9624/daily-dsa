5b138e110fd2e87680c26f9f3735da2f0f2f2ae6
Sagar B R <sagarsagar34176@gmail.com>
2026-05-05 17:54:25 +0530

    Implement preorder traversal for binary tree

---
+++ day13.py
@@ -0,0 +1,24 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def preorder(root):
+    if root is None:
+        return
+    print(root.data, end=" ")
+    preorder(root.left)
+    preorder(root.right)
+
+if __name__ == "__main__":
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.left.right.right = Node(8)
+    preorder(root)

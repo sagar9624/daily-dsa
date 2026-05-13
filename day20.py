5abb8a4d498bdb4373253c0f26d203c5cff89757
Sagar B R <sagarsagar34176@gmail.com>
2026-05-13 09:58:52 +0530

    Implement cycle detection in an undirected graph

---
+++ day20.py
@@ -0,0 +1,28 @@
+class solution:
+    def dfs(self,cur,parent,adj,visited):
+        visited[cur]=True 
+        for i in adj[cur]:
+            if i !=parent:
+                if visited[i]:
+                    return True 
+                elif self.dfs(i,cur,adj,visited):
+                    return True 
+        return False 
+        
+    def checkCycle(self,n,adj):
+        visited=[False]*n 
+        for i in range(n):
+            if not visited[i]:
+                if self.dfs(i,-1,adj,visited):
+                    return True 
+        return False 
+    
+
+if __name__=="__main__":
+  n,e=map(int,input().split()) 
+  adj=[[]for _ in range(n)]
+  for _ in range(e):
+    u,v=map(int,input().split()) 
+    adj[u].append(v) 
+    adj[v].append(u) 
+  print("True" if checkCylce(n,adj) else"False")

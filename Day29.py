bc4523d34303f450f9ef16380dc0babd94947886
Sagar B R <sagarsagar34176@gmail.com>
2026-05-22 17:33:16 +0530

    Add DFS algorithm to find safe nodes
    Implement depth-first search to find safe nodes in a directed graph.

---
+++ Day29.py
@@ -0,0 +1,45 @@
+def dfs(curr, adj, visited, path, ans):
+    visited[curr] = 1
+    path[curr] = 1
+    
+    for k in adj[curr]:
+        if not visited[k]:
+            if dfs(k, adj, visited, path, ans):
+                return True
+        elif path[k]:
+            return True
+            
+    path[curr] = 0
+    ans.append(curr)
+    return False
+
+def safeNodes(adj):
+    n = len(adj)
+    visited = [0] * n
+    path = [0] * n
+    ans = []
+    
+    for i in range(n):
+        if not visited[i]:
+            dfs(i, adj, visited, path, ans)
+    
+    ans.sort()
+    return ans
+
+
+if __name__ == "__main__":
+    n, e = map(int, input().split())
+
+    adj = [[] for _ in range(n)]
+
+    for _ in range(e):
+        u, v = map(int, input().split())
+        adj[u].append(v)
+
+    result = safeNodes(adj)
+    
+    if len(result) == 0:
+        print("[]")
+    else:
+        for i in result:
+            print(i, end=" ")

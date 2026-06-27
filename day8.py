62a5a1d2e2f05b5a8100d07eef092caa8fa842dc
Sagar B R <sagarsagar34176@gmail.com>
2026-06-27 17:43:15 +0530

    Fix syntax error in loop condition

--- day8.py
+++ day8.py
@@ -10,7 +10,7 @@ def cycleDetection(head):
     while fast is not None and fast.next is not None:
         slow = slow.next  
         fast = fast.next.next
-        if slow == fast: af
+        if slow == fast:
             return True
     return False
 

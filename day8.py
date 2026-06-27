0a3fa7bf9edfaef90acc2d6e8cc580751393416c
Sagar B R <sagarsagar34176@gmail.com>
2026-06-27 17:43:05 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -8,9 +8,9 @@ def cycleDetection(head):
     slow = head
     fast = head
     while fast is not None and fast.next is not None:
-        slow = slow.next
+        slow = slow.next  
         fast = fast.next.next
-        if slow == fast:
+        if slow == fast: af
             return True
     return False
 

0cfb6ab3d2fd3ccba459ae39beb48b17efa2e5ff
Sagar B R <sagarsagar34176@gmail.com>
2026-07-21 17:44:43 +0530

    Fix syntax error in root node initialization

--- day13.py
+++ day13.py
@@ -13,7 +13,7 @@ def preorder(root):
     preorder(root.right)
 
 if __name__ == "__main__":
-    root = Node(1) asfdgh
+    root = Node(1) 
     root.left = Node(2)
     root.right = Node(3)
     root.left.left = Node(4)

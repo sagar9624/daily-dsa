cb7e27fd415f142855a3dc2daa0f9c98c39dd78f
Sagar B R <sagarsagar34176@gmail.com>
2026-07-26 11:19:43 +0530

    Fix selectionSort function by removing junk text
    Removed extraneous text from the selection sort function.

--- day1.cpp
+++ day1.cpp
@@ -7,7 +7,7 @@ void selectionSort(int arr[],int n){
   for(int i=0;i<=n-1;i++){
     int min_index=i;
     for(int j=i+1;j<n;j++){
-      if(arr[j]<arr[min_index])min_index=j; asdfg
+      if(arr[j]<arr[min_index])min_index=j; 
   }
     int temp=arr[i];
     arr[i]=arr[min_index];
